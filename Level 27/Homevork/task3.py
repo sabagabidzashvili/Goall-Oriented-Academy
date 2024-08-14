c555f353cb97f999a62d6c3a02d8daa38db36b3d
sabagabidzashvili <sabagabidzashvili97@gmail.com>
2024-08-14 19:29:53 +0400

    commit

---
+++ Level 27/Homevork/task3.py
@@ -0,0 +1,3 @@
+listn = [121,344,66,79,3,6]
+for i in listn:
+    print(i * 2)
---
+++ Level 27/Homevork/task4.py
@@ -0,0 +1,9 @@
+numbers = [2, 3, 5, 7, 11]
+
+multiplication = 1
+
+
+for number in numbers:
+   multiplication *= number  
+
+print(multiplication)
